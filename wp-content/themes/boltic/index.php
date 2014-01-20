5b78aaf62561f6126950803d0616df1a51b1ca21
Daniel Friberg <dfriberg86@gmail.com>
2014-01-20 11:51:16 +0100

    Ta bort tom länk på framsidan sponsorer

--- wp-content/themes/boltic/index.php
+++ wp-content/themes/boltic/index.php
@@ -83,33 +83,30 @@ query_posts("post_type=post&posts_per_page=1"); ?>
 	<?php while (have_posts()) : the_post(); ?>
 		
 		<li>
-			<a href="<?php the_permalink(); ?>">
+			<?php
+			$post_meta_data = get_post_custom($post->ID); //Hämta meta-box fälten för att använda nedan
 
-				<?php
-				$post_meta_data = get_post_custom($post->ID); //Hämta meta-box fälten för att använda nedan
+			// Om sponsorn har en dedikerad sida, länka till den
+			if( get_post_meta($post->ID, 'sponsor_dedikerad_sida', true) ){
+				echo '<a href="';
+				the_permalink();
+				echo '">';
+			} else{ // Om sponsorn inte har en dedikerad sida
+				//Om sponsorn har en hemsida, länka dit
+				if( get_post_meta($post->ID, 'sponsor_url', true) )
+					echo '<a href="'. $post_meta_data['sponsor_url'][0] .'" target="_blank">';
+			}
 
-				// Om sponsorn har en dedikerad sida, länka till den
-				if( get_post_meta($post->ID, 'sponsor_dedikerad_sida', true) ){
-					echo '<a href="';
-					the_permalink();
-					echo '">';
-				} else{ // Om sponsorn inte har en dedikerad sida
-					//Om sponsorn har en hemsida, länka dit
-					if( get_post_meta($post->ID, 'sponsor_url', true) )
-						echo '<a href="'. $post_meta_data['sponsor_url'][0] .'" target="_blank">';
-				}
+			// Visa logotyp
+			// Ändra "thumbnail" till den storlek som önskas
+			echo wp_get_attachment_image($post_meta_data['sponsor_logo'][0], 'sponsor-logo');
 
-				// Visa logotyp
-				// Ändra "thumbnail" till den storlek som önskas
-				echo wp_get_attachment_image($post_meta_data['sponsor_logo'][0], 'sponsor-logo');
+			echo '<p>'. get_the_title() .'</p>';
 
-				echo '<p>'. get_the_title() .'</p>';
-
-				// Stäng länk om det finns en dedikerad sida eller hemsida
-				if( get_post_meta($post->ID, 'dedikerad_sida', true) || get_post_meta($post->ID, 'sponsor_url', true) )
-					echo '</a>';
-				?>
-			</a>
+			// Stäng länk om det finns en dedikerad sida eller hemsida
+			if( get_post_meta($post->ID, 'dedikerad_sida', true) || get_post_meta($post->ID, 'sponsor_url', true) )
+				echo '</a>';
+			?>
 		</li>
 
     <?php endwhile; ?>
